2af4353e8ddb222457e7d2970504c80e69a2c8c3
Roni Choudhury <roni.choudhury@kitware.com>
2017-03-14 21:58:36 -0400

    WIP
    - Back out this commit and remove the stuff to make the electron
    executable "work"
    
    - Implement test image resizing to allow for different settings to
    produce different sizes of test images

--- shell.nix
+++ shell.nix
@@ -2,24 +2,51 @@
 
 with pkgs;
 
-stdenv.mkDerivation {
+let libs = [
+  alsaLib
+  atk
+  cairo
+  cups
+  dbus
+  expat
+  fontconfig.lib
+  fontconfig.dev
+  gdk_pixbuf
+  glib
+  glibc
+  gnome3.gconf
+  gtk2
+  nspr
+  nss
+  #libxcb
+  pango
+  freetype
+  stdenv.cc.cc.lib
+  zlib
+] ++ (with xorg; [
+  libX11
+  libxcb
+  libXcomposite
+  libXcursor
+  libXdamage
+  libXext
+  libXi
+  libXfixes
+  libXrandr
+  libXrender
+  libXScrnSaver
+  libXtst
+]);
+in stdenv.mkDerivation {
   name = "candela";
 
   buildInputs = [
+    electron
     nodejs
-    fontconfig.lib
-    fontconfig.dev
-    freetype
-    zlib
-  ];
+  ] ++ libs;
 
   C_INCLUDE_PATH = "${fontconfig.dev}/include";
-  LD_LIBRARY_PATH = builtins.concatStringsSep ":" (map (x: "${x}/lib") [
-    zlib
-    fontconfig.lib
-    freetype
-    stdenv.cc.cc.lib
-  ]);
+  LD_LIBRARY_PATH = stdenv.lib.makeLibraryPath libs;
 
   shellHook = ''
     echo "Candela nix dev environment"
